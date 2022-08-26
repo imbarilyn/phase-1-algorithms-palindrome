fdeb1e0c4b92cb3ba0f7927d55a82876cfa33960
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-08-26 14:58:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
+  const putInArray = word.split("");
+  const reverseArray = putInArray.reverse();
+  const reverseString = reverseArray.join("");
+  if(word === reverseString){
+    return true;
+  }
+  else{
+    return false;
+  }
+
   // Write your algorithm here
 }
+isPalindrome("abba");
+
 
 /* 
   Add your pseudocode here
+ 1. first I write the string in an array
+ 2. I will reverse the array element
+ 3. I change the array into a string
+ 4. I check if the word is same as the reversed string
+
 */
 
 /*
   Add written explanation of your solution here
+  What to here is to take the string and convert it to an array then revese the array using the method Array.prototype.reverse() then change the array into a string using Array.prototype.join() method then compare it with the string given if they are the same it is a palindrome otherwise not palindrome
 */
 
 // You can run `node index.js` to view these console logs
